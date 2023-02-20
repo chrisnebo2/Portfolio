a482b62cc08f66f15b2df07320f7b5274ff18c23
Christian Nebo <nebochristian9@gmail.com>
2023-02-20 21:23:46 +0100

    My first comit, getting up to speed

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+function NavBar() {
+    const [toggle, setToggle] = useState(false)
+    const myLinks = [
+        {
+            id: 1,
+            link: "home"
+        },
+        {
+            id: 2,
+            link: "about"
+        },
+        {
+            id: 3,
+            link: "experience"
+        },
+        {
+            id: 4,
+            link: "portfolio"
+        },
+        {
+            id: 5,
+            link: "contact"
+        }
+    ]
+    
+  return (
+    <div className="w-full bg-black text-white px-4 md:px-6 h-20 flex items-center justify justify-between fixed top-0">
+        <div>
+            <h1 className="text-4xl md:text-5xl font-logo">Chris</h1>
+        </div>
+        <ul className="hidden md:flex">
+            {
+                myLinks.map(({link, id}) => (
+                    <li key={id} className="capitalize font-medium px-4 text-gray-400 hover:scale-105 duration-200 ease-out cursor-pointer hover:text-gray-200">{link}</li>
+                ))
+            }
+        </ul>
+        <div className="text-gray-500 pr-4 z-10 cursor-pointer md:hidden">
+            {
+                toggle ? 
+                <FaTimes size={25} onClick={() => setToggle(false)} /> :
+                <FaBars size={25} onClick={() => setToggle(true)}/>
+            }
+        </div>
+        {toggle && (
+            <ul className="md:hidden w-full h-screen bg-gradient-to-b from-black to-gray-600 absolute top-0 left-0 flex flex-col justify-center items-center">
+                {
+                    myLinks.map(({link, id}) => (
+                        <li key={id} className="capitalize text-2xl py-2 text-gray-400 hover:scale-105 cursor-pointer hover:text-gray-200">{link}</li>
+                    ))
+                }
+            </ul>
+        )}
+
+    </div>
+  )
+}
+
+export default NavBar;
